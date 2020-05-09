74bed3fbb454a57b1268a8c532fe9b173f0493e3
MarcusMcKenzie <46785875+MarcusMcKenzie@users.noreply.github.com>
2020-05-09 18:50:25 -0400

    Add files via upload

---
+++ nvProject/fastMCD/build/CMakeFiles/fastMCD.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/kivi/Desktop/MotionDetection/fastMCD/src/KLTWrapper.cpp" "/home/kivi/Desktop/MotionDetection/fastMCD/build/CMakeFiles/fastMCD.dir/src/KLTWrapper.cpp.o"
+  "/home/kivi/Desktop/MotionDetection/fastMCD/src/MCDWrapper.cpp" "/home/kivi/Desktop/MotionDetection/fastMCD/build/CMakeFiles/fastMCD.dir/src/MCDWrapper.cpp.o"
+  "/home/kivi/Desktop/MotionDetection/fastMCD/src/main.cpp" "/home/kivi/Desktop/MotionDetection/fastMCD/build/CMakeFiles/fastMCD.dir/src/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "."
+  "../"
+  "../."
+  "../src"
+  "/opt/ros/kinetic/include/opencv-3.3.1-dev"
+  "/opt/ros/kinetic/include/opencv-3.3.1-dev/opencv"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ nvProject/fastMCD/build/CMakeFiles/fastMCD.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/fastMCD.dir/src/main.cpp.o"
+  "CMakeFiles/fastMCD.dir/src/KLTWrapper.cpp.o"
+  "CMakeFiles/fastMCD.dir/src/MCDWrapper.cpp.o"
+  "fastMCD.pdb"
+  "fastMCD"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/fastMCD.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
